e9b2cc71b4cb6851efe51c70330a86c22f85dc38
hasibul-af <hasibul.af@gmail.com>
2023-10-18 03:51:07 +0600

    Got Error in 56_5-5 (User not Deleted from Database)

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,7 +1,26 @@
+import { useState } from "react";
 import { useLoaderData } from "react-router-dom";
 
 const Users = () => {
   const loadedUsers = useLoaderData();
+  const [users, setUsers] = useState(loadedUsers);
+
+  const handleDelete = (id) => {
+    // Make sure user is confirm to delete
+    fetch(`http://localhost:5174/user/${id}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        if (data.deletedCount > 0) {
+          console.log("deleted successfully");
+          // remove the user from the UI
+          const remainingUsers = users.filter((user) => user._id !== id);
+          setUsers(remainingUsers);
+        }
+      });
+  };
+
   return (
     <div>
       <h2>Users: {loadedUsers.length}</h2>
@@ -13,18 +32,24 @@ const Users = () => {
               <th></th>
               <th>Email</th>
               <th>Creates At </th>
+              <th>Last Logged in</th>
               <th>Action</th>
             </tr>
           </thead>
           <tbody>
-            {
-                loadedUsers.map(user => <tr key={user._id}>
-                    <th>1</th>
-                    <td>{user.email}</td>
-                    <td>{user.createdAt}</td>
-                    <td>None</td>
-                  </tr>)
-            }
+            {users.map((user) => (
+              <tr key={user._id}>
+                <th>1</th>
+                <td>{user.email}</td>
+                <td>{user.createdAt}</td>
+                <td>{user.lastLoggedAt}</td>
+                <td>
+                  <button
+                    onClick={() => handleDelete(user._id)}
+                    className="btn">X</button>
+                </td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
